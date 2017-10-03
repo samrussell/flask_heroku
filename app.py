93fa89c528df46ff3d09f843ddede49bf072e804
Sam Russell <sam.h.russell@gmail.com>
2017-10-03 22:04:35 +1300

    use templates

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def index():
+  things = [
+          { 'type' : 'food', 'name' : 'banana' },
+          { 'type' : 'food', 'name' : 'tofu' },
+          { 'type' : 'bike', 'name' : 'suzuki' },
+          ]
+  return render_template('hello.html', things=things)
 
